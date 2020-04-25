388ec07921bcc1a7d24d9dcb8bc94dba1aeadcc7
Rodrigo Rodrigues <serdasteclas@gmail.com>
2020-04-25 19:27:03 -0300

    add dataset base for template

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -5,3 +5,7 @@ from app import app
 @app.route('/')
 def index():
     return render_template('default.html')
+
+@app.route('/add_dataset')
+def add_dataset():
+    return render_template('add_dataset.html')
