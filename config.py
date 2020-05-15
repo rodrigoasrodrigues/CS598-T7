a0e55c41cb55fbc0020a1fa6b4a117b7da1d537a
Rodrigo Rodrigues <serdasteclas@gmail.com>
2020-05-15 04:25:15 -0300

    switching to AzureMSSQL

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 DEBUG = False
-SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+SQLALCHEMY_DATABASE_URI = "mssql+pyodbc://w2vec_app:8sajrnw7YTGETGn@w2v.database.windows.net/w2vec?driver=SQL+Server"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
