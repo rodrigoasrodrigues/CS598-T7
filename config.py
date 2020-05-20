29c9a428d9144a3974e3007da043a30f18e0ba00
Rodrigo Rodrigues <serdasteclas@gmail.com>
2020-05-20 16:19:48 -0300

    constring

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 DEBUG = False
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
-SQLALCHEMY_DATABASE_URI = "mssql+pyodbc://w2vec_app:8sajrnw7YTGETGn@w2v.database.windows.net/w2vec?driver=ODBC+Driver+17+for+SQL+Server"
+SQLALCHEMY_DATABASE_URI = "mssql+pyodbc://w2v:Develop@123@w2v.database.windows.net/w2v?driver=ODBC+Driver+17+for+SQL+Server"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
